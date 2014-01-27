a5805a84dbd0efb1099e1ea3db8f5e899d77faf3
TomKim2012 <tosh0948@gmail.com>
2014-01-27 15:50:42 +0300

    Temporal Commit for Dashboard without Icons

---
+++ src/main/java/com/tomkimani/mgwt/demo/client/ui/FeaturedButton.java
@@ -0,0 +1,13 @@
+package com.tomkimani.mgwt.demo.client.ui;
+
+import com.googlecode.mgwt.ui.client.MGWT;
+import com.googlecode.mgwt.ui.client.MGWTStyle;
+import com.googlecode.mgwt.ui.client.theme.base.TabBarCss;
+import com.googlecode.mgwt.ui.client.widget.tabbar.FeaturedTabBarButton;
+
+public class FeaturedButton extends FeaturedTabBarButton {
+		
+	public FeaturedButton(TabBarCss css) {
+		//FeaturedTabBarButton(css,MGWTStyle.getTheme().getMGWTClientBundle().tabBarFeaturedImage());
+	}
+}
